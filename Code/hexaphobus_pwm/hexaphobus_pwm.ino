4bec17a72ddf9f864045b68d4136d17f1eaaedaf
oliroy92 <oliroy92@gmail.com>
2020-02-05 16:27:41 -0500

    Arduino update

--- Code/hexaphobus_pwm/hexaphobus_pwm.ino
+++ Code/hexaphobus_pwm/hexaphobus_pwm.ino
@@ -7,6 +7,13 @@ Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
 #define MAX_PULSE_WIDTH       2650
 #define DEFAULT_PULSE_WIDTH   1500
 #define FREQUENCY             50
+
+#define UP 45
+#define DOWN 0
+#define FRONT 90 
+#define BACK 0
+#define CENTER (FRONT-BACK)/2+BACK
+  
 // our servo # counter
 uint8_t servonum = 0;
 
@@ -17,41 +24,12 @@ void setup() {
 }
 
 void loop() {
-  
-  pwm.setPWM(0, 0, pulseWidth(0));
-  pwm.setPWM(1, 0, pulseWidth(0));
-  pwm.setPWM(6, 0, pulseWidth(0));
-  pwm.setPWM(7, 0, pulseWidth(0));
-  pwm.setPWM(8, 0, pulseWidth(0));
-  pwm.setPWM(9, 0, pulseWidth(0));
-  pwm.setPWM(10, 0, pulseWidth(0));
-  pwm.setPWM(11, 0, pulseWidth(0));
-  pwm.setPWM(12, 0, pulseWidth(0));
-  pwm.setPWM(13, 0, pulseWidth(0));
-  pwm.setPWM(14, 0, pulseWidth(0));
-  pwm.setPWM(15, 0, pulseWidth(0));
-  delay(2000);
-  pwm.setPWM(0, 0, pulseWidth(0));
-  pwm.setPWM(1, 0, pulseWidth(0));
-  pwm.setPWM(6, 0, pulseWidth(0));
-  pwm.setPWM(7, 0, pulseWidth(0));
-  pwm.setPWM(8, 0, pulseWidth(0));
-  pwm.setPWM(9, 0, pulseWidth(0));
-  pwm.setPWM(10, 0, pulseWidth(0));
-  pwm.setPWM(11, 0, pulseWidth(0));
-  pwm.setPWM(12, 0, pulseWidth(0));
-  pwm.setPWM(13, 0, pulseWidth(0));
-  pwm.setPWM(14, 0, pulseWidth(0));
-  pwm.setPWM(15, 0, pulseWidth(0));
-  delay(2000);
+  ForwardAndBackwards(1,4,5,0);
+  UpAndDown(1,4,5,1);
+  ForwardAndBackwards(1,4,5,1);
+  UpAndDown(1,4,5,0);
 
-  //test=angleWidth(650);
-
-
-  
-  
-  //Serial.println(angle_moteur);
-  //angleWidth(angle_moteur);
+  init_mouv();
   
 }
 
@@ -60,14 +38,111 @@ int pulseWidth(int angle)
   int pulse_wide, analog_value;
   pulse_wide   = map(angle, 0, 180, MIN_PULSE_WIDTH, MAX_PULSE_WIDTH);
   analog_value = int(float(pulse_wide) / 1000000 * FREQUENCY * 4096);
-  Serial.println(analog_value);
   return analog_value;
 }
 
-int angleWidth(int pulse)
+int AnalogToAngle(int analog_value)
 {
-  int angle_wide, analog_value;
-  angle_wide = map(pulse, MIN_PULSE_WIDTH, MAX_PULSE_WIDTH, 0 ,180);
-  Serial.println("L'angle du moteur");
+  int angle_wide, pulse_wide;
+  pulse_wide = int(float(analog_value) * 1000000 / FREQUENCY / 4096);
+  angle_wide = map(pulse_wide, MIN_PULSE_WIDTH, MAX_PULSE_WIDTH, 0 ,180);
   return angle_wide;
 }
+
+void init_mouv()
+{
+  UpAndDown(1,4,5,0)
+  UpAndDown(2,3,6,1)
+  UpAndDown(2,3,6,1)
+  
+}
+
+//Up = 0
+//Down = 1
+//Leg= de 1 à 6 
+void UpAndDown(int Leg1,int Leg2,int Leg3,int pos)
+{
+     //modifier l'angle
+     int Legs [3] = {Leg1, Leg2, Leg3};
+     int angle;
+     if (pos==0)
+     {
+        angle=DOWN; 
+     }
+     else
+     {
+        angle=UP;
+     }
+     
+     for (int i=0; i<3; i++)
+     {
+       if (Legs[i]%2 == 0)
+       {
+          pwm.setPWM(Legs[i]+6, 0, pulseWidth(180-angle));
+       }
+       
+       else
+       {
+          pwm.setPWM(Legs[i]+6, 0, pulseWidth(angle));
+       }
+     }
+     delay(500);
+}
+
+//Back = 0
+//Front = 1
+//Leg= de 1 à 6
+//gérer cas d'erreur 
+void ForwardAndBackwards(int Leg1,int Leg2,int Leg3,int pos)
+{
+   //modifier l'angle
+   int Legs [3] = {Leg1, Leg2, Leg3};
+   int angle;
+   if (pos==0)
+   {
+      angle=BACK; 
+   }
+   else if(pos==1)
+   {
+      angle=CENTER;
+   }
+   else 
+   {
+      angle=FRONT;
+   }
+   
+   for (int i=0; i<3; i++)
+   {
+     if (Legs[i]%2 == 0)
+     {
+        pwm.setPWM(Legs[i], 0, pulseWidth(180-angle));
+     }
+     
+     else
+     {
+        pwm.setPWM(Legs[i], 0, pulseWidth(angle));
+     }
+   }
+   delay(500);
+} 
+   
+    
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
