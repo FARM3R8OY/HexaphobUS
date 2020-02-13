52b57046c454054469fcb1ce5ef39ddadd46615f
oliroy92 <oliroy92@gmail.com>
2020-02-13 16:29:13 -0500

    fonction plus modulaire

--- Code/hexaphobus_pwm/hexaphobus_pwm.ino
+++ Code/hexaphobus_pwm/hexaphobus_pwm.ino
@@ -41,9 +41,9 @@ S4-H20 | GRO400
 //angle pour chaque position
 #define UP 110
 #define DOWN 92
-#define FRONT 90
-#define BACK 160
-#define CENTER 125
+#define FRONT 170
+#define BACK 90
+#define CENTER 120
   
 // our servo # counter
 uint8_t servonum = 0;
@@ -61,25 +61,21 @@ void setup() {
 
 void loop() {
 
-  UpAndDown(1,4,6,POS_UP);
-  ForwardAndBackwards(1,4,6,POS_FRONT);
-  delay(1000);
-  ForwardAndBackwards(1,4,6,POS_BACK);
-  delay(2000);
-  ForwardAndBackwards(1,4,6,POS_CENTER);
-  UpAndDown(1,4,6,POS_DOWN);
-  delay(1000);
- /*init_mouv();   
- delay(2000);
- int B_MovingForward=1;
+
+ init_mouv();   
+ delay(50000);
+ /*int B_MovingForward=1;
  MovingForward(B_MovingForward,3);
- delay(10000);
+ delay(1000);
  B_MovingForward=0;
  int B_MovingBackwards=1;
- MovingBackwards(B_MovingBackwards,2);
+ MovingBackwards(B_MovingBackwards,3);
  delay(5000);
  B_MovingBackwards=0;
- delay(200000);*/
+ delay(2000);
+ int B_MovingRight=1;
+ MovingRight(B_MovingRight,3);
+ B_MovingRight=0;*/
 }
 
 //Passer d'une valeur en angle à une valeur analogue
@@ -104,42 +100,43 @@ int AnalogToAngle(int analog_value)
 //Les jambes sont à leur position initiale
 void init_mouv()
 {
-  UpAndDown(1,4,5,POS_DOWN);
-  UpAndDown(2,3,6,POS_UP);
-  ForwardAndBackwards(2,3,6,POS_CENTER);
-  UpAndDown(2,3,6,POS_DOWN);
-  UpAndDown(1,4,5,POS_UP);
-  ForwardAndBackwards(1,4,5,POS_CENTER);
-  UpAndDown(1,4,5,POS_DOWN);
+  UpAndDown(1,4,5,POS_DOWN,POS_DOWN,POS_DOWN);
+  UpAndDown(1,4,5,POS_DOWN,POS_DOWN,POS_DOWN);
+  UpAndDown(2,3,6,POS_UP,POS_UP,POS_UP);
+  ForwardAndBackwards(2,3,6,POS_CENTER,POS_CENTER,POS_CENTER);
+  UpAndDown(2,3,6,POS_DOWN,POS_DOWN,POS_DOWN);
+  UpAndDown(1,4,5,POS_UP,POS_UP,POS_UP);
+  ForwardAndBackwards(1,4,5,POS_CENTER,POS_CENTER,POS_CENTER);
+  UpAndDown(1,4,5,POS_DOWN,POS_DOWN, POS_DOWN);
 }
-
+//------------------------------------- Fonction de sequencement du mouvement-------------------------------------------
 //Fonction permettant au robot d'avancer tant que le bouton est activé
 //ajouter le bouton de linterface
 void MovingForward(int B_MovingForward,int nb_sequence)
 {
     while (B_MovingForward<nb_sequence)
     {
-      UpAndDown(2,3,6,POS_UP);
-      ForwardAndBackwards(2,3,6,POS_FRONT);
-      ForwardAndBackwards(2,3,6,POS_BACK);
-      UpAndDown(2,3,6,POS_DOWN);
+      UpAndDown(2,3,6,POS_UP,POS_UP,POS_UP);
+      ForwardAndBackwards(2,3,6,POS_FRONT,POS_FRONT,POS_FRONT);
+      ForwardAndBackwards(1,4,5,POS_BACK,POS_BACK,POS_BACK);
+      UpAndDown(2,3,6,POS_DOWN,POS_DOWN,POS_DOWN);
       while (B_MovingForward<nb_sequence)
       {
-        UpAndDown(1,4,5,POS_UP);
-        ForwardAndBackwards(1,4,5,POS_FRONT);
-        ForwardAndBackwards(2,3,6,POS_BACK);
-        UpAndDown(1,4,5,POS_DOWN);        
+        UpAndDown(1,4,5,POS_UP,POS_UP,POS_UP);
+        ForwardAndBackwards(1,4,5,POS_FRONT,POS_FRONT,POS_FRONT);
+        ForwardAndBackwards(2,3,6,POS_BACK,POS_BACK,POS_BACK);
+        UpAndDown(1,4,5,POS_DOWN,POS_DOWN,POS_DOWN);        
         
-        UpAndDown(2,3,6,POS_UP);
-        ForwardAndBackwards(2,3,6,POS_FRONT);
-        ForwardAndBackwards(2,3,6,POS_BACK);
-        UpAndDown(2,3,6,POS_DOWN);
+        UpAndDown(2,3,6,POS_UP,POS_UP,POS_UP);
+        ForwardAndBackwards(2,3,6,POS_FRONT,POS_FRONT,POS_FRONT);
+        ForwardAndBackwards(1,4,5,POS_BACK,POS_BACK,POS_BACK);
+        UpAndDown(2,3,6,POS_DOWN,POS_DOWN,POS_DOWN);
         B_MovingForward+=B_MovingForward;
       }
-      UpAndDown(1,4,5,POS_UP);
-      ForwardAndBackwards(1,4,5,POS_FRONT);
-      ForwardAndBackwards(2,3,6,POS_BACK);
-      UpAndDown(1,4,5,POS_DOWN); 
+      UpAndDown(1,4,5,POS_UP,POS_UP,POS_UP);
+      ForwardAndBackwards(1,4,5,POS_FRONT,POS_FRONT,POS_FRONT);
+      ForwardAndBackwards(2,3,6,POS_BACK,POS_BACK,POS_BACK);
+      UpAndDown(1,4,5,POS_DOWN,POS_DOWN,POS_DOWN); 
       init_mouv();
     }
 }
@@ -150,68 +147,103 @@ void MovingBackwards(int B_MovingBackwards,int nb_sequence)
 {
     while (B_MovingBackwards<nb_sequence)
     {
-      UpAndDown(2,3,6,POS_UP);
-      ForwardAndBackwards(2,3,6,POS_BACK);
-      ForwardAndBackwards(1,4,5,POS_FRONT);
-      UpAndDown(2,3,6,POS_DOWN);
+      UpAndDown(2,3,6,POS_UP,POS_UP,POS_UP);
+      ForwardAndBackwards(2,3,6,POS_BACK,POS_BACK,POS_BACK);
+      ForwardAndBackwards(1,4,5,POS_FRONT,POS_FRONT,POS_FRONT);
+      UpAndDown(2,3,6,POS_DOWN,POS_DOWN,POS_DOWN);
       while (B_MovingBackwards<nb_sequence)
       {
-        UpAndDown(1,4,5,POS_UP);
-        ForwardAndBackwards(1,4,5,POS_BACK);
-        ForwardAndBackwards(2,3,6,POS_FRONT);
-        UpAndDown(1,4,5,POS_DOWN);        
+        UpAndDown(1,4,5,POS_UP,POS_DOWN,POS_DOWN);
+        ForwardAndBackwards(1,4,5,POS_BACK,POS_BACK,POS_BACK);
+        ForwardAndBackwards(2,3,6,POS_FRONT,POS_FRONT,POS_FRONT);
+        UpAndDown(1,4,5,POS_DOWN,POS_DOWN,POS_DOWN);        
         
-        UpAndDown(2,3,6,POS_UP);
-        ForwardAndBackwards(2,3,6,POS_BACK);
-        ForwardAndBackwards(1,4,5,POS_FRONT);
-        UpAndDown(2,3,6,POS_DOWN);
+        UpAndDown(2,3,6,POS_UP,POS_UP,POS_UP);
+        ForwardAndBackwards(2,3,6,POS_BACK,POS_BACK,POS_BACK);
+        ForwardAndBackwards(1,4,5,POS_FRONT,POS_FRONT,POS_FRONT);
+        UpAndDown(2,3,6,POS_DOWN,POS_DOWN,POS_DOWN);
         B_MovingBackwards+=B_MovingBackwards;
       }
       
-      UpAndDown(1,4,5,POS_UP);
-      ForwardAndBackwards(1,4,5,POS_BACK);
-      ForwardAndBackwards(2,3,6,POS_FRONT);
-      UpAndDown(1,4,5,POS_DOWN); 
+      UpAndDown(1,4,5,POS_UP,POS_UP,POS_UP);
+      ForwardAndBackwards(1,4,5,POS_BACK,POS_BACK,POS_BACK);
+      ForwardAndBackwards(2,3,6,POS_FRONT,POS_FRONT,POS_FRONT);
+      UpAndDown(1,4,5,POS_DOWN,POS_DOWN,POS_DOWN); 
       init_mouv();
     }
 }
 
+//Fonction pour tourner vers la droite
+void MovingRight(int B_MovingRight,int nb_sequence)
+{
+    while (B_MovingRight<nb_sequence)
+    {
+      UpAndDown(2,3,6,POS_UP, POS_UP,POS_UP);
+      ForwardAndBackwards(2,3,6,POS_FRONT,POS_BACK,POS_FRONT);
+      ForwardAndBackwards(1,4,5,POS_FRONT,POS_BACK,POS_FRONT);
+      UpAndDown(2,3,6,POS_DOWN,POS_DOWN,POS_DOWN);
+      while (B_MovingRight<nb_sequence)
+      {
+        UpAndDown(7,10,11,POS_UP,POS_UP,POS_UP);
+        ForwardAndBackwards(1,4,5,POS_BACK,POS_FRONT,POS_BACK);
+        ForwardAndBackwards(2,3,6,POS_BACK,POS_FRONT,POS_BACK);
+        UpAndDown(1,4,5,POS_DOWN,POS_DOWN,POS_DOWN);        
+        
+        UpAndDown(2,3,6,POS_UP,POS_UP,POS_UP);
+        ForwardAndBackwards(2,3,6,POS_FRONT,POS_BACK,POS_FRONT);
+        ForwardAndBackwards(1,4,5,POS_FRONT,POS_BACK,POS_FRONT);
+        UpAndDown(2,3,6,POS_DOWN,POS_DOWN,POS_DOWN);
+        
+        B_MovingRight+=B_MovingRight;
+      }
+      
+        UpAndDown(7,10,11,POS_UP,POS_UP,POS_UP);
+        ForwardAndBackwards(1,4,5,POS_BACK,POS_FRONT,POS_BACK);
+        ForwardAndBackwards(2,3,6,POS_BACK,POS_FRONT,POS_BACK);
+        UpAndDown(1,4,5,POS_DOWN,POS_DOWN,POS_DOWN);
+    } 
+  
+  
+}
 
+void MovingLeft()
+{
+  
+}
 
+//------------------------------------ Fonction de mouvement -------------------------------------------
 
 //Active les moteurs pour lever ou descendre 3 jambes, 2 d'un côté, 1 de l'autre en même temps
 //Up = 1
 //Down = 0
 //Passer 3 Leg en argument de 1 à 6 
+//passer les trois positions des legs 
 //gérer cas d'erreur ----------------------------------------
-void UpAndDown(int Leg1,int Leg2,int Leg3,int pos)
+void UpAndDown(int Leg1,int Leg2,int Leg3,int pos1,int pos2,int pos3)
 {
 
      int Legs [3] = {Leg1, Leg2, Leg3};
-     int angle;
-     if (pos==0)
-     {
-        angle=DOWN; 
-     }
-     else
-     {
-        angle=UP;
-     }
+     int pos [3] = {pos1,pos2,pos3};
+     int angle[3]={0,0,0};
 
      for (int i=0; i<3; i++)
      {
-       Serial.print(angle);
+       if (pos[i]==0)
+       { angle[i]=DOWN;}
+       else
+       { angle[i]=UP;}
+
        if (Legs[i]%2 == 0)
        {
-          pwm.setPWM(Legs[i]+6, 0, pulseWidth(180-angle));
+          pwm.setPWM(Legs[i]+6, 0, pulseWidth(180-angle[i]));
        }
        
        else
        {
-          pwm.setPWM(Legs[i]+6, 0, pulseWidth(angle));
+          pwm.setPWM(Legs[i]+6, 0, pulseWidth(angle[i]));
        }
      }
-     delay(500);
+     delay(300);
 }
 
 
@@ -221,39 +253,32 @@ void UpAndDown(int Leg1,int Leg2,int Leg3,int pos)
 //Center = 2 ou autres
 //Passer 3 Leg en argument de 1 à 6 
 //gérer cas d'erreur ----------------------------------------
-void ForwardAndBackwards(int Leg1,int Leg2,int Leg3,int pos)
+void ForwardAndBackwards(int Leg1,int Leg2,int Leg3,int pos1, int pos2, int pos3)
 {
    //modifier l'angle
    int Legs [3] = {Leg1, Leg2, Leg3};
-   int angle;
-   if (pos==0)
-   {
-      angle=BACK; 
-   }
-   /*else if (pos==1)
-   {
-      angle=FRONT;
-   }*/
-   else
-   {
-      angle=CENTER;
-   }
-   Serial.print("angle fetB /n");
-   Serial.print(angle);
+   int pos[3] = {pos1, pos2, pos3};
+   int angle[3]={0,0,0};
    for (int i=0; i<3; i++)
    {
-
+    if (pos[i]==0)
+      {angle[i]=BACK;}
+    else if (pos[i]==1)
+      {angle[i]=FRONT;}
+    else
+      {angle[i]=CENTER;}
+   
      if (Legs[i]%2 == 0)
      {
-        pwm.setPWM(Legs[i], 0, pulseWidth(180-angle));
+        pwm.setPWM(Legs[i], 0, pulseWidth(180-angle[i]));
      }
      
      else
      {
-        pwm.setPWM(Legs[i], 0, pulseWidth(angle));
+        pwm.setPWM(Legs[i], 0, pulseWidth(angle[i]));
      }
    }
-   delay(500);
+   delay(300);
 } 
 
 
