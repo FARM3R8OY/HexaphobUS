950c61e3d2120c07e014210655e7d9ee5fbbbbd9
gabrielcabana21 <cabg2101@usherbrooke.ca>
2020-03-19 15:09:19 -0400

    Updated documentation.

--- docs/html/search/functions_1.js
+++ docs/html/search/functions_1.js
@@ -1,5 +1,5 @@
 var searchData=
 [
-  ['addservos_36',['addServos',['../d9/d57/classhexaphobus__ui_1_1hexaphobus__ui_1_1_main_window.html#a4f0f2eafc34b6f0bdf1c901d25ca4a40',1,'hexaphobus_ui::hexaphobus_ui::MainWindow']]],
-  ['addwidgets_37',['addWidgets',['../d9/d57/classhexaphobus__ui_1_1hexaphobus__ui_1_1_main_window.html#a25940b92ffef6344606195c145b31aaf',1,'hexaphobus_ui::hexaphobus_ui::MainWindow']]]
+  ['addservos_38',['addServos',['../d9/d57/classhexaphobus__ui_1_1hexaphobus__ui_1_1_main_window.html#a4f0f2eafc34b6f0bdf1c901d25ca4a40',1,'hexaphobus_ui::hexaphobus_ui::MainWindow']]],
+  ['addwidgets_39',['addWidgets',['../d9/d57/classhexaphobus__ui_1_1hexaphobus__ui_1_1_main_window.html#a25940b92ffef6344606195c145b31aaf',1,'hexaphobus_ui::hexaphobus_ui::MainWindow']]]
 ];
